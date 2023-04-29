1ece1b7615a4c91db9c0e6e58b46ccffb55ea815
hamzaatouri <86068901+hamzaatouri@users.noreply.github.com>
2023-04-29 11:18:53 +0200

    Update ExamThourayaS2ApplicationTests.java

--- DEVOPS/src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
+++ DEVOPS/src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTests.java
@@ -1,13 +1,57 @@
-//package tn.esprit.spring;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class ExamThourayaS2ApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package tn.esprit.spring;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Slf4j
+class ExamThourayaS2ApplicationTests {
+
+@Test
+public 	void testAddTrain()
+
+     {
+  
+  TrainRepository TR = Mockito.mock(TrainRepository.class);
+  Train t = new Train(5L,etat.en_gare,70);
+  Mockito.when(TR.save(t)).thenReturn(t);
+  assertNotNull(t);
+		
+
+      }
+
+}
+
+
+
+
+
+
+
+	
+	
+
+
