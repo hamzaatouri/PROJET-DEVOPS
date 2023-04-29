fab88ddc26675542907b5ee5a4c31f7c8fb3a629
hamzaatouri <86068901+hamzaatouri@users.noreply.github.com>
2023-04-29 12:27:07 +0200

    Update ExamThourayaS2ApplicationTest.java

--- DEVOPS/src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTest.java
+++ DEVOPS/src/test/java/tn/esprit/spring/ExamThourayaS2ApplicationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
 
 
 
